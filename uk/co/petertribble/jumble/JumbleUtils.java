425d6e50f8cafc5fa5ce3fbcffc8eccf1aee7bde
Peter Tribble <peter.tribble@gmail.com>
2024-11-02 13:45:38 +0000

    More convenient methods stringToPropMap(String s), fileToPropMap(File f)

--- uk/co/petertribble/jumble/JumbleUtils.java
+++ uk/co/petertribble/jumble/JumbleUtils.java
@@ -11,7 +11,7 @@
 package uk.co.petertribble.jumble;
 
 import java.util.*;
-import java.io.*;
+import java.io.File;
 
 /**
  * Convenience routines.
@@ -25,6 +25,39 @@ public final class JumbleUtils {
      * This class cannot be instantiated, or subclassed.
      */
     private JumbleUtils() {
+    }
+
+    /**
+     * Reads a File and creates a property Map.
+     * The lines in the File are broken up into a key
+     * and value, with the key being anything to the left of the first
+     * '=' character and the value anything that is left over. If no '='
+     * character is present, that line is skipped.
+     *
+     * @param f The <code>File</code> to be read.
+     *
+     * @return A <code>Map</code> containing the keys and values
+     * specified by the input <code>String</code>
+     */
+    public static Map<String, String> fileToPropMap(File f) {
+	return stringToPropMap(JumbleFile.getStringContents(f));
+    }
+
+    /**
+     * Converts a <code>String</code> to a property Map. The
+     * <code>String</code> is first broken up into lines.
+     * Then the resulting lines are broken up into a key
+     * and value, with the key being anything to the left of the first
+     * '=' character and the value anything that is left over. If no '='
+     * character is present, that <code>String</code> is skipped.
+     *
+     * @param s The <code>String</code> to be broken up.
+     *
+     * @return A <code>Map</code> containing the keys and values
+     * specified by the input <code>String</code>
+     */
+    public static Map<String, String> stringToPropMap(String s) {
+	return stringToPropMap(s, "\n");
     }
 
     /**
